88df20a0fc87b8408ba97b2d12a2122d09f71cf2
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-11 07:51:19 -0300

    chore: finished user creation case flow

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,8 +1,38 @@
+import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 
-export class CreateUserCase {
-  constructor(private usersRepository: IUsersRepository) {}
+export class CreateUserUseCase {
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
-  async execute(data: ICreateUserRequestDTO) {}
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists.');
+    }
+
+    const user = new User(data);
+
+    await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        email: data.email,
+        name: data.name,
+      },
+      from: {
+        email: 'my@service.com',
+        name: 'my.service',
+      },
+      subject: `Thank's for registration !`,
+      body: '<h3>Your registration was successfully !</h3>',
+    });
+  }
 }
